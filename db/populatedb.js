22ddc502e4eb91bbaf395dd7624b157db2ddc4b7
jdogcodey <jonnydawson@hotmail.com>
2024-12-18 23:06:52 +0000

    Set up script to create DB

---
+++ db/populatedb.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+require("dotenv").config();
+
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR ( 255 )
+);
+
+INSERT INTO usernames (username) 
+VALUES
+  ('Bryan'),
+  ('Odin'),
+  ('Damon');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: `postgresql://${user}:${password}@localhost:5432/top_users`,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
